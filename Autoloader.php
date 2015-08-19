3bd6906fc51c2bc8994f56e8787f8d9840ff08fc
david.burkhart <david.burkhart@hotmail.com>
2015-08-20 01:11:20 +0200

    ~changed autoloader based on the APPLICATION_PATH

--- Autoloader.php
+++ Autoloader.php
@@ -3,20 +3,11 @@
 class Autoloader {
 
 	public static function load($targetClass) {
-		$backtrace = debug_backtrace();
-		$backtraceIndex = isset($backtrace[2]) ? 2 : 1;
-		$partsFileCalledFrom = explode(DIRECTORY_SEPARATOR, dirname($backtrace[1]['file']));
-		$partsClassCalledFrom = array();
-		if (isset($backtrace[$backtraceIndex]['class'])) {
-			$partsClassCalledFrom = explode('\\', $backtrace[$backtraceIndex]['class']);
+		if (!defined('APPLICATION_PATH')) {
+			throw new \Exception('APPLICATION_PATH is not set!');
 		}
 		$partsTargetClass = explode('\\', $targetClass);
-		$partsAppDir = self::cutArrayEnd($partsFileCalledFrom, $partsClassCalledFrom);
-
-		$partsAppDir = self::cutArrayEnd($partsAppDir, $partsTargetClass);
-
-		$path = implode(DIRECTORY_SEPARATOR, array_merge($partsAppDir, $partsTargetClass)).'.php';
-
+		$path = APPLICATION_PATH.DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR, $partsTargetClass).'.php';
 		if (file_exists($path)) {
 			require_once $path;
 			if (!class_exists($targetClass)) {
@@ -25,15 +16,5 @@ class Autoloader {
 		} else {
 			throw new \Exception('File \''.$path.'\' does not exist!'.print_r(debug_backtrace(),1));
 		}
-	}
-
-	private static function cutArrayEnd($arrayCut, $arrayEnd) {
-		$arrayFinal = $arrayCut;
-		for ($i = 0; $i < count($arrayCut); $i++) {
-			if (isset($arrayEnd[0]) && $arrayEnd[0] === $arrayCut[$i] && !empty($arrayEnd[0])) {
-				$arrayFinal = array_splice($arrayCut, 0, $i);
-			}
-		}
-		return $arrayFinal;
 	}
 }
---
+++ AutoloaderOld.php
@@ -0,0 +1,43 @@
+<?php
+
+class Autoloader {
+
+	public static function load($targetClass) {
+		$backtrace = debug_backtrace();
+		$backtraceIndexFile = isset($backtrace[1]['file']) ? 1 : 2;
+		$backtraceIndexClass = isset($backtrace[2]) ? 2 : 1;
+		$partsFileCalledFrom = explode(DIRECTORY_SEPARATOR, dirname($backtrace[$backtraceIndexFile]['file']));
+		$partsClassCalledFrom = array();
+
+		if (isset($backtrace[$backtraceIndexClass]['class'])) {
+			$partsClassCalledFrom = explode('\\', $backtrace[$backtraceIndexClass]['class']);
+		}
+
+		$partsTargetClass = explode('\\', $targetClass);
+
+		$partsAppDir = self::cutArrayEnd($partsFileCalledFrom, $partsClassCalledFrom);
+		$partsAppDir = self::cutArrayEnd($partsAppDir, $partsTargetClass);
+		$path = implode(DIRECTORY_SEPARATOR, array_merge($partsAppDir, $partsTargetClass)).'.php';
+		if (file_exists($path)) {
+			foreach (glob(dirname($path).'/*.php') as $filename) {
+				include $filename;
+			}
+			require_once $path;
+			if (!class_exists($targetClass)) {
+				throw new \Exception('Class \''.$targetClass.'\' does not exist!'.print_r(debug_backtrace(),1));
+			}
+		} else {
+			throw new \Exception('File \''.$path.'\' does not exist!'.print_r(debug_backtrace(),1));
+		}
+	}
+
+	private static function cutArrayEnd($arrayCut, $arrayEnd) {
+		$arrayFinal = $arrayCut;
+		for ($i = 0; $i < count($arrayCut); $i++) {
+			if (isset($arrayEnd[0]) && $arrayEnd[0] === $arrayCut[$i] && !empty($arrayEnd[0])) {
+				$arrayFinal = array_splice($arrayCut, 0, $i);
+			}
+		}
+		return $arrayFinal;
+	}
+}
