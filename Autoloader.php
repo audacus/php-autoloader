41238ad83d9fce03129ef52183d0f2f351b3029a
audacus <david.burkhart@hotmail.com>
2015-09-21 09:26:30 +0200

    ~hey there! :D

--- Autoloader.php
+++ Autoloader.php
@@ -2,6 +2,10 @@
 
 class Autoloader {
 
+	const FILE_NOT_FOUND = 1;
+	const CLASS_NOT_FOUND = 2;
+	private static $error;
+
 	public static function load($targetClass) {
 		if (!defined('APPLICATION_PATH')) {
 			throw new \Exception('APPLICATION_PATH is not set!');
@@ -12,10 +16,30 @@ class Autoloader {
 		if (file_exists($path)) {
 			require_once $path;
 			if (!class_exists($targetClass)) {
+				self::setError(self::CLASS_NOT_FOUND);
 				throw new \Exception('Class \''.$targetClass.'\' does not exist!'.print_r(debug_backtrace(),1));
 			}
 		} else {
+			self::setError(self::FILE_NOT_FOUND);
 			throw new \Exception('File \''.$path.'\' does not exist!'.print_r(debug_backtrace(),1));
 		}
+	}
+
+	/**
+	* Gets the value of error.
+	*
+	* @return mixed
+	*/
+	public static function getError() {
+		return self::$error;
+	}
+
+	/**
+	* Sets the value of error.
+	*
+	* @param mixed $error the error
+	*/
+	private static function setError($error){
+		self::$error = $error;
 	}
 }
